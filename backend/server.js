ba69025f10ef2fc9a3e122b16c8fe678ada570c7
Bård Aase <elzapp@gmail.com>
2018-12-12 19:05:08 +0100

    Updated cv

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,17 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/me', (request, response) => {
-	response.send('{"name": "Arthur Dent", "location": "Earth"}');
+	response.send('{"name": "Bård Aase", "location": "Norway"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "2018", "yearTo": "Now", "place": "Sbanken", "comment": "DevOps"}, {"yearFrom": "2009", "yearTo": "2018", "place": "Vizrt", "comment": "Developer"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
 app.get('/api/education', (request, response) => {
-	response.send('[{"yearFrom": "2005", "yearTo": "2007", "place": "University 1", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]');
+	response.send('[{"yearFrom": "2003", "yearTo": "2006", "place": "Universitetet i Bergen", "comment": "BS Computer Technology"}]');
 	logger.info('Responded to GET request on /api/edu')
 });
 
